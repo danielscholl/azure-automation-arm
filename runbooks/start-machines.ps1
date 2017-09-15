12915346b0a349fccc8c3ac3ebb484ecd8588feb
Daniel Scholl <daniel@scholllopez.com>
2017-09-15 09:22:08 -0500

    Fixing Runbooks to add in Scale Sets

--- runbooks/start-machines.ps1
+++ runbooks/start-machines.ps1
@@ -8,48 +8,69 @@
     .NOTES
         AUTHOR: Daniel Scholl
 #>
-workflow start-machines {
-  Param(
-    [string]$ResourceGroupName
-  )
 
-  inlineScript {
-    $ResourceGroupName = $using:ResourceGroupName
+Param(
+  [string]$ResourceGroupName
+)
 
-    $connectionName = "AzureRunAsConnection"
-    try {
-      # Get the connetion  "AzureRunAsConnection"
-      $servicePrincipalConnection = Get-AutomationConnection -Name $connectionName
+$connectionName = "AzureRunAsConnection"
 
-      "Logging in to Azure..."
-      Add-AzureRmAccount `
-        -ServicePrincipal `
-        -TenantId $servicePrincipalConnection.TenantId `
-        -ApplicationId $servicePrincipalConnection.ApplicationId `
-        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint
-    }
-    catch {
-      if (!$servicePrincipalConnection) {
-        $ErrorMessage = "Connection $connectionName not found."
-        throw $ErrorMessage
-      }
-      else {
-        Write-Error -Message $_.Exception
-        throw $_.Exception
-      }
-    }
+try {
+  #---------Read the Credentials variable---------------
+  # Get the connetion  "AzureRunAsConnection"
+  $servicePrincipalConnection = Get-AutomationConnection -Name $connectionName
 
-    $Machines = Get-AzureRmVM -ResourceGroupName $ResourceGroupName
+  #-----L O G I N - A U T H E N T I C A T I O N-----
+  Write-Output "Logging into Azure Account..."
 
-    if (!$Machines) {
-      Write-Output "No VMs found in the Resource Group."
-    }
-    else {
-      $Machines | ForEach-Object {
-        Write-Output "Starting Server $_.Name"
-        Start-AzureRMVM -Name $_.Name `
-          -ResourceGroupName $ResourceGroupName
-      }
-    }
+
+  Add-AzureRmAccount `
+    -ServicePrincipal `
+    -TenantId $servicePrincipalConnection.TenantId `
+    -ApplicationId $servicePrincipalConnection.ApplicationId `
+    -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint
+
+  Write-Output "Successfully logged into Azure Subscription..."
+}
+catch {
+  if (!$servicePrincipalConnection) {
+    $ErrorMessage = "Connection $connectionName not found."
+    throw $ErrorMessage
+  }
+  else {
+    Write-Error -Message $_.Exception
+    throw $_.Exception
+  }
+}
+
+#---------Start Virtual Machines---------------
+Write-Output "Retrieving Virtual Machines..."
+$Machines = Get-AzureRmVM -ResourceGroupName $ResourceGroupName
+
+if (!$Machines) {
+  Write-Output "No Virtual Machines found in the Resource Group."
+}
+else {
+  $Machines | ForEach-Object {
+    $name = $_.Name
+    Write-Output "Starting Server $name"
+    Start-AzureRMVM -Name $name `
+      -ResourceGroupName $ResourceGroupName
+  }
+}
+
+
+#---------Start Virtual Machine Scale Sets---------------
+$Machines = Get-AzureRmVmss -ResourceGroupName $ResourceGroupName
+
+if (!$Machines) {
+  Write-Output "No VIrtual Machine Scale Sets found in the Resource Group."
+}
+else {
+  $Machines | ForEach-Object {
+    $name = $_.Name
+    Write-Output "Starting Scale Set $name"
+    Start-AzureRMVmss -Name $name `
+      -ResourceGroupName $ResourceGroupName
   }
 }
